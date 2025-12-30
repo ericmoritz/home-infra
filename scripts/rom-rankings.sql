4706b35dfda190ee2dc91f5624ab74dc78f4d12f
Eric Codes <eric@ericcodes.io>
2025-12-29 22:59:27 -0500

    tweaked the top n query

--- scripts/rom-rankings.sql
+++ scripts/rom-rankings.sql
@@ -1,28 +1,39 @@
 -- This updated my top roms collection with the top roms for every platform.
-delete from collections_roms where collection_id = 3;
+delete from collections_roms where collection_id = 4;
 
-with latest_roms as (select r.igdb_id,
-                            r.id,
-                            r.name,
-                            p.slug as platform,
-                            md.average_rating,
-                            row_number() over (partition by r.igdb_id
-                                                   order by r.revision desc,
-                                                            r.id desc) as rom_order
-                       from roms r
-                 inner join roms_metadata md on r.id = md.rom_id
-                 inner join platforms p on p.id = r.platform_id
-                      where average_rating > 0),
+with latest_roms as (
+        select r.igdb_id,
+               r.id,
+               r.name,
+               p.slug as platform,
+               md.average_rating as rating,
+               row_number() over (partition by r.igdb_id
+                                      order by r.revision desc,
+                                               r.id desc) as rom_order
+          from roms r
+    inner join roms_metadata md on r.id = md.rom_id
+    inner join platforms p on p.id = r.platform_id),
 
-     ranked_roms as (select id as rom_id,
-                     row_number() over (partition by platform order by average_rating desc) as rank
-                     from latest_roms
-                     where rom_order = 1)
+     ranked_roms as (
+       select id as rom_id,
+              name as rom_name,
+              rating,
+              platform,
+              row_number() over (partition by platform
+                                     order by rating desc) as rank
+         from latest_roms
+        where rom_order = 1
+          and rating > 0
+          and rating <> 100)
 
+--   select *
+--     from ranked_roms
+--    where rank <= 10
+-- order by platform, rank;
 
 insert into collections_roms
 (collection_id, rom_id)
-select 3 as collection_id,
+select 4 as collection_id,
        rom_id
  from ranked_roms
-where rank <= 10;
+where rank <= 25;
